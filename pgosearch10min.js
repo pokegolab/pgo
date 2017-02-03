a9e761b171c81d7acf3e5056524f126d59925812
pokegolab <pokego.labo@gmail.com>
2017-02-03 22:09:57 +0900

    Add files via upload

---
+++ pgosearch10min.js
@@ -0,0 +1,6 @@
+$("#map-canvas").append("<div style='border: 1px solid #aaa;width: 30px;height: 30px;top: 0;bottom: 0;left: 0;right: 0;position: absolute;margin: auto; z-index: 1000; border-radius: 50%;'></div>");
+setInterval(
+function(){
+$("#area_buttonsearch").click();
+},
+1000);
